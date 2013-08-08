9d219604fdfbbf9cdc304794481dcb863f4286b5
Corey Trombley <corey.trombley33@gmail.com>
2013-08-08 12:18:43 -0400

    getting rates from api

--- app/models/shipping_label.rb
+++ app/models/shipping_label.rb
@@ -3,7 +3,7 @@ class ShippingLabel < ActiveRecord::Base
   belongs_to :to_address, :class_name => 'Address', :foreign_key => :to_address_id
   belongs_to :shipping_rate #TODO - Make a model called ShippingRate
 
-  attr_accessible :from, :item, :to, :weight, :label_url, :from_address_attributes, :to_address_attributes, :date
+  attr_accessible :from, :item, :to, :weight, :label_url, :from_address_attributes, :to_address_attributes, :ship_date
 
   accepts_nested_attributes_for :from_address, :to_address
 
@@ -16,7 +16,7 @@ class ShippingLabel < ActiveRecord::Base
       :from_zip_code => '45440',
       :to_zip_code   => '20500',
       :weight_lb     => '0.5',
-      :ship_date     => self.date
+      :ship_date     => self.ship_date
     )
 
     stamp = Stamps.create!({
