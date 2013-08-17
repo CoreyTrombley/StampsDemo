001b287295d5ab76688c8c94b3cf8a4ea3496eb2
Corey Trombley <corey.trombley33@gmail.com>
2013-08-17 10:09:01 -0400

    working on validator

--- lib/add_on_validator.rb
+++ lib/add_on_validator.rb
@@ -2,9 +2,9 @@ class AddOnValidator < ActiveModel::Validator
   def validate(o)
     if o.rate
       o.rate[:add_ons][:add_on_v4].each do |add_on|
-        if add_on[:requires_all_of] 
+        if add_on[:requires_all_of]
           if add_on[:requires_all_of][:requires_one_of][:add_on_type_v4].include?(o.add_on_codes)
-            #
+            
           else
           o.errors[:add_ons] << "You must include one of the following codes: #{add_on[:requires_all_of][:requires_one_of][:add_on_type_v4]}"
           end
