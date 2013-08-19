a13b1f2f33cf7c42d1dc9eae1e7cf1ac91bf666f
Corey Trombley <corey.trombley33@gmail.com>
2013-08-19 10:13:48 -0400

    validator problems

--- lib/add_on_validator.rb
+++ lib/add_on_validator.rb
@@ -2,14 +2,28 @@ class AddOnValidator < ActiveModel::Validator
   def validate(o)
     if o.rate
       o.rate[:add_ons][:add_on_v4].each do |add_on|
-        if add_on[:requires_all_of]
-          if add_on[:requires_all_of][:requires_one_of][:add_on_type_v4].include?(o.add_on_codes)
-            
-          else
-          o.errors[:add_ons] << "You must include one of the following codes: #{add_on[:requires_all_of][:requires_one_of][:add_on_type_v4]}"
+        if add_on[:requires_all_of] || add_on[:prohibited_with_any_of]
+          if add_on[:requires_all_of]
+            required = add_on[:requires_all_of][:requires_one_of][:add_on_type_v4]
+            if required.class == String
+              array = []
+              array << required
+              required = array
+            end
+            if (required & o.add_on_codes)
+              if (required & o.add_on_codes).empty?
+                o.errors[:add_ons] << "You must include one of the following codes: #{required}"
+              end
+            end
           end
-        elsif add_on[:prohibited_with_any_of][:add_on_type_v4].include?(o.add_on_codes)
-          o.errors[:add_ons] << "You can not include any of the following codes: #{add_on[:prohibited_with_any_of][:add_on_type_v4]}"
+          if add_on[:prohibited_with_any_of]
+            prohibited = add_on[:prohibited_with_any_of][:add_on_type_v4]
+            if (prohibited & o.add_on_codes)
+              if (prohibited & o.add_on_codes).present?
+                o.errors[:add_ons] << "You can not include any of the following codes: #{prohibited}"
+              end
+            end
+          end
         end
       end
     end
